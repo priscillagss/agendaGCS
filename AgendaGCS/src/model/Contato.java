2279af8f3b6693e32e1ebb32509ae13153b91341
Yeltsin <yeltsin.lobato@hotmail.com>
2014-09-10 15:23:34 -0300

    modificando model contato

--- AgendaGCS/src/model/Contato.java
+++ AgendaGCS/src/model/Contato.java
@@ -3,17 +3,19 @@ package model;
 public class Contato {
 
 	private String nome;
-	private int telefone;
+	private int telefone_fixo;
 	private String email;
+	private int telefone_celular;
 	
-	public Contato(String nome, int telefone, String email) {
+	public Contato(String nome, int telefone_fixo, String email, int telefone_celular) {
 		this.nome = nome;
-		this.telefone = telefone;
+		this.telefone_fixo = telefone_fixo;
 		this.email = email;
+		this.telefone_celular = telefone_celular; 
 	}
 	
 	public Contato() {
-		super();
+		
 	}
 
 	public String getNome() {
@@ -24,12 +26,20 @@ public class Contato {
 		this.nome = nome;
 	}
 
-	public int getTelefone() {
-		return telefone;
+	public int getTelefone_fixo() {
+		return telefone_fixo;
 	}
 
-	public void setTelefone(int telefone) {
-		this.telefone = telefone;
+	public void setTelefone_fixo(int telefone_fixo) {
+		this.telefone_fixo = telefone_fixo;
+	}
+
+	public int getTelefone_celular() {
+		return telefone_celular;
+	}
+
+	public void setTelefone_celular(int telefone_celular) {
+		this.telefone_celular = telefone_celular;
 	}
 
 	public String getEmail() {
